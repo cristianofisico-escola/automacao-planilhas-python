c621c8e84fa4b55f37f9bdc3902aa671d9f296b2
cristianofisico-escola <129231786+cristianofisico-escola@users.noreply.github.com>
2026-02-17 12:58:19 -0300

    Backup Rápido: 17/02/2026 12:58

--- ligar.sh
+++ ligar.sh
@@ -1,1 +1,8 @@
-streamlit run app.py
+# =========================================================================
+# PARA LIGAR O SISTEMA (FLASK), COPIE E COLE O COMANDO ABAIXO NO TERMINAL:
+# ./ligar.sh
+# =========================================================================
+
+#!/bin/bash
+echo "🚀 Iniciando o servidor Flask..."
+python3 app.py
